d67ea3d99b8d9462de3d5406e7f8c3dd884b87e0
choo <chutaeking@gmail.com>
2022-12-29 22:34:18 +0900

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,4 +2,5 @@ export * from './Home';
 export * from './Photo';
 export * from './Room';
 export * from './Rooms';
-// index 파일을 폴더 마다 만들어 둔 이유는 - import { Home, Photo, Room, Rooms } from "./pages"; 이처럼 라이브러리 처럼 쓰일수 있기 떄문이다.
+// index 파일을 폴더 마다 만들어 둔 이유는 - import { Home, Photo, Room, Rooms } from "./pages"; 
+// 이처럼 라이브러리 처럼 쓰일수 있기 떄문이다.
